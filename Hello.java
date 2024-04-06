a020e26eac11fb4b00edc55c56a481e7e1f0fe21
Lakers-23 <451618843@qq.com>
2024-04-06 20:46:27 +0800

    开始

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+   //      
+   public static void main(String[] args) {
+      System.out.println("hello java word!");
+     }
+}
+
+//编译后，每一个类都会生成一个.class文件
+class Dog{
+   public static void main(String[] args) {
+      System.out.println("hello!");
+   }
+}
